e422162489fcb583d4dd252c8e30ad6bfe6abe21
Dani <zsikla.dani@gmail.com>
2020-05-28 18:32:01 +0200

    Frontend-hez igazítva egy kicsit.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -11,10 +11,16 @@ insert into Building (b_name, floors) values ('Eszaki tomb', 8);
 insert into Building (b_name, floors) values ('PPK Epulet', 9);
 
 
-insert into Subject (name, code, description, credit, building_id) values ('Alkalmazások fejlesztése', 'IK01', 'Spring boot és Angular fejlesztés',1, 3);
-insert into Subject (name, code, description, credit, building_id) values ('Valószínűségszámítás', 'IK02', 'Beleépítve statisztika',2, 2);
-insert into Subject (name, code, description, credit, building_id) values ('Analízis 3.', 'IK03', 'Integrálás', 3, 1);
-insert into Subject (name, code, description, credit, building_id) values ('Analízis 1.', 'AN01', 'Deriválás alapjai', 3, 3);
+insert into Subject (name, code, description, credit, building_id,status) values ('Alkalmazások fejlesztése', 'IK01', 'Spring boot és Angular fejlesztés',1, 10,'Első félév');
+insert into Subject (name, code, description, credit, building_id,status) values ('Valószínűségszámítás', 'IK02', 'Beleépítve statisztika',2, 2,'Első félév');
+insert into Subject (name, code, description, credit, building_id,status) values ('Analízis 3.', 'IK03', 'Integrálás', 3, 1,'Első félév');
+insert into Subject (name, code, description, credit, building_id,status) values ('Analízis 1.', 'AN01', 'Deriválás alapjai', 3, 3,'Első félév');
+insert into Subject (name, code, description, credit, building_id,status) values ('Prog alap', 'pr10', 'For ciklus', 3, 3,'Első félév');
+insert into Subject (name, code, description, credit, building_id,status) values ('Gt prog', 'pr11', 'Gt lib', 3, 3,'Második félév');
+insert into Subject (name, code, description, credit, building_id,status) values ('Java', 'pr12', 'Java bla-bla', 3, 3,'Harmadik félév');
+insert into Subject (name, code, description, credit, building_id,status) values ('C++', 'pr13', 'C++ mély megsimerése', 3, 3,'Negyedik félév');
+insert into Subject (name, code, description, credit, building_id,status) values ('Valószínűség számítás', 'IK33', 'Kombinatorika egyetemi szinten', 3, 3,'Ötödik félév');
+insert into Subject (name, code, description, credit, building_id,status) values ('MI', 'pr15', 'Mesterséges ...', 3, 3,'Hatodik félév');
 
 
 insert into users_subjects (users_id, subjects_id) values (1, 1);
